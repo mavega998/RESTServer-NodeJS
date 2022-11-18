e17205bbb373213fcc2d3da069d993d8f3f99ca8
Michael A. Vega Pabon <mavega998@misena.edu.co>
2022-11-18 08:52:33 -0500

    feat: add to new services for users.

--- server.js
+++ server.js
@@ -1,32 +1,21 @@
-require('./config/config');
-
+const { config } = require('./config/config')
 const express = require('express');
+const cors = require('cors')
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors());
 
-app.set('port', process.env.PORT);
+app.set('port', config.port);
 
-app.get('/user', (req, res) => {
-    res.send('ok');
+app.get('/', (req, res) => {
+    res.send('Welcome!');
 });
 
-app.post('/user', (req, res) => {
-    let newUser = req.body;
-    res.json(newUser);
-});
-
-app.put('/user/:id', (req, res) => {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-
-app.delete('/user', (req, res) => {
-    res.send('ok');
-});
+// Routes
+app.use('/api/users', require('./routes/user.router.js'));
 
 app.listen(app.get('port'), () => {
     console.log(`Server run on port ${app.get('port')}`);
